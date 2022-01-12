2d64bb002af5f13cbd38ca5853c213fdbe650537
AbdulBari7077 <a.bari.ab64@gmail.com>
2022-01-12 23:54:54 +0500

    done

--- PDC/MPI/views.py
+++ PDC/MPI/views.py
@@ -1,56 +1,101 @@
-from django.http import HttpResponse 
+from django.http import HttpResponse
 from django.shortcuts import render
 import os
 import time
+import random
+
 
 def home(request):
     args1 = {"operation": "getRegionData"}
     os.system('mpiexec -n 4 python PDCProject-master/script.py "' + str(args1) + '"')
-    file=open("PDCProject-master/temp.txt","r")
+    file = open("PDCProject-master/temp.txt", "r")
     data = file.read().split("\n")
     # print("-------data-------",data)
     file.close()
-    Array=[]
-    data=data[0:len(data)-1]
+    Array = []
+    chartStr = []
+    data = data[0 : len(data) - 1]
     for i in data:
-        line=i.split("\t")
-        # print(line[0],line[1])
-        dit={'zipCode':line[0],'zipName':line[1],'positiveCount':line[2],'totalCount':line[3],'positiveRatio':(round((int(line[2])/int(line[3]))*100)),'vaccinationCount':line[4]}
+        line = i.split("\t")
+       
+       
+        dit = {
+            "zipCode": line[0],
+            "zipName": line[1],
+            "positiveCount": line[2],
+            "totalCount": line[3],
+            "positiveRatio": (round((int(line[2]) / int(line[3])) * 100)),
+            "vaccinationCount": line[4],
+        }
+
+        chartElem = {
+            "zipname": (line[1]),
+            "positiveCount": int(line[2])*10000,
+            "vaccinationCount": int(line[4])*10000,
+        }
+        chartStr.append(chartElem)
         Array.append(dit)
-    dict={'list':Array}
-    # print("--------------",dict)
-    return render(request,"home.html",dict)
+
+    dict = {"list": Array, "chartStr": str(chartStr)}
+
+    return render(request, "home.html", dict)
+
 
 def userDetails(request):
-    Array=[]
+    Array = []
     if request.method == "POST":
-        zipcode=request.POST.get("zipcode")
-        minAge=request.POST.get("minAge")
-        maxAge=request.POST.get("maxAge")
-        if minAge=="" and maxAge=="":
-            minAge=0
-            maxAge=1000
+        zipcode = request.POST.get("zipcode")
+        minAge = request.POST.get("minAge")
+        maxAge = request.POST.get("maxAge")
+        cnic = request.POST.get("cnic")
+        gender = request.POST.get("gender")
+        print("Gender", gender)
+        if minAge == "" and maxAge == "":
+            minAge = 0
+            maxAge = 1000
+        elif minAge == "" or maxAge == "":
+            if minAge == "":
+                minAge = 0
+            if maxAge == "":
+                maxAge = 1000
+
         args = {
             "operation": "getUserInfo",
-            "params": {"zipCode": zipcode, "minAge": minAge, "maxAge": maxAge},
+            "params": {
+                "zipCode": zipcode,
+                "minAge": minAge,
+                "maxAge": maxAge,
+                "cnic": cnic,
+                "gender": gender,
+            },
         }
         os.system('mpiexec -n 4 python PDCProject-master/script.py "' + str(args) + '"')
-        # print(zipcode,minAge,maxAge)
+
         file = open("PDCProject-master/temp.txt", "r")
         data = file.read().split("\n")
         file.close()
-        data=data[0:len(data)-1]
-        counter=0
+        data = data[0 : len(data) - 1]
+        counter = 0
         for i in data:
-            counter=counter+1
-            line=i.split("\t")
+            counter = counter + 1
+            line = i.split("\t")
             # print(line[0],line[1])
-            if line[6]=="notvaccinated":
-                line[6]="not vaccinated"
-            dit={'Sno':counter,'Name':line[0],'Age':line[1],'Cnic':line[2],'Gender':line[3],'Street':line[4],'ZipCode':line[5],'Vaccinestatus':line[6],'CovidStatus':line[7]}
+            if line[6] == "notvaccinated":
+                line[6] = "not vaccinated"
+
+            dit = {
+                "Sno": counter,
+                "Name": line[0],
+                "Age": line[1],
+                "Cnic": line[2],
+                "Gender": line[3],
+                "Street": line[4],
+                "ZipCode": line[5],
+                "Vaccinestatus": line[6],
+                "CovidStatus": line[7],
+            }
             Array.append(dit)
-        # print(request.POST.get("zipcode"))
-        # list =[{'fname':'abc', 'lname':'cadsad', 'cnic':'321321-21312-231'},{'fname':'abfdsfsdc', 'lname':'cadsafsdfdsd', 'cnic':'34321321-21312-231'}]
-        dict={'list':Array}
-        return render(request,"userDetails.html", dict)
-    return render(request,"userDetails.html")
+
+        dict = {"list": Array}
+        return render(request, "userDetails.html", dict)
+    return render(request, "userDetails.html")
--- PDC/PDCProject-master/helper.py
+++ PDC/PDCProject-master/helper.py
@@ -1,25 +1,32 @@
 from datetime import date
 from mpi4py import MPI
+
 # from types import NoneType
-def getRegionCount(vaccinationCount,totalCount,regionList, data):
+def getRegionCount(vaccinationCount, totalCount, regionList, data):
     # regionIndex is the array index where region name is stored
     regionIndex = 6
-    covidStatusIndex=8
+    covidStatusIndex = 8
     for line in data:
         line = line.split("\t")
-        if str(line[7])=="vaccinated":
-            vaccinationCount[line[regionIndex]] = vaccinationCount[line[regionIndex]] + 1
-        if str(line[8])=="positive":
-            regionList[line[regionIndex]] = regionList[line[regionIndex]] + 1
-            totalCount[line[regionIndex]]= totalCount[line[regionIndex]] +1
+        if str(line[7]) == "vaccinated":
+            vaccinationCount[line[regionIndex]] = (
+                vaccinationCount[line[regionIndex]] + 1
+            )
+        if str(line[8]) == "positive":
+            regionList[line[regionIndex]] = regionList[line[regionIndex]] + 1 #covid positive
+            totalCount[line[regionIndex]] = totalCount[line[regionIndex]] + 1
         else:
-            totalCount[line[regionIndex]]= totalCount[line[regionIndex]] +1
+            totalCount[line[regionIndex]] = totalCount[line[regionIndex]] + 1
 
-    dict={"vaccinationCount":vaccinationCount,"pCount":regionList,"totalCount":totalCount}
+    dict = {
+        "vaccinationCount": vaccinationCount,
+        "pCount": regionList,
+        "totalCount": totalCount,
+    }
     return dict
 
 
-def getUserDetails(data, options,comm):
+def getUserDetails(data, options, comm):
 
     zipCodeIndex = 6
     # ageIndex = 1
@@ -27,23 +34,117 @@ def getUserDetails(data, options,comm):
     zipCode = options["zipCode"]
     minAge = int(options["minAge"])
     maxAge = int(options["maxAge"])
-
+    cnic = options["cnic"]
+    gender = int(options["gender"])
+    print("cnic rank", cnic, comm.rank)
+    print("gender", gender)
     userDetails = []
-    for line in data:
-        line = line.split("\t")
-        bdate=line[1].split("-")
-        today = date.today()
-        age=today.year - int(bdate[0]) - ((today.month, today.day) < (int(bdate[1]), int(bdate[2])))
-        if (zipCode == ""):
-            if (age >= minAge and age <= maxAge):
 
-                dict={'Name':line[0],'Age':age,'Cnic':line[2],'Gender':line[3],'Street':line[5],'ZipCode':line[6],'Vaccinestatus':line[7],'CovidStatus':line[8]}
+    if cnic != "":
+        for line in data:
+            # for k in range(0, comm.size):
+                # if comm.irecv(None, source=k):
+                #     break
+            line = line.split("\t")
+            bdate = line[1].split("-")
+            today = date.today()
+            age = (
+                today.year
+                - int(bdate[0])
+                - ((today.month, today.day) < (int(bdate[1]), int(bdate[2])))
+            )
+            if line[2] == str(cnic):
+                print("Data Found", cnic, comm.rank)
+                dict = {
+                    "Name": line[0],
+                    "Age": age,
+                    "Cnic": line[2],
+                    "Gender": line[3],
+                    "Street": line[5],
+                    "ZipCode": line[6],
+                    "Vaccinestatus": line[7],
+                    "CovidStatus": line[8],
+                }
                 userDetails.append(dict)
-            # int(line[ageIndex]) >= minAge
-            # int(line[ageIndex]) <= maxAge
-        if (line[zipCodeIndex] == str(zipCode)and age >= minAge and age <= maxAge):
-            dict={'Name':line[0],'Age':age,'Cnic':line[2],'Gender':line[3],'Street':line[5],'ZipCode':line[6],'Vaccinestatus':line[7],'CovidStatus':line[8]}
-            userDetails.append(dict)
+                # for j in range(0, comm.size):
+                #     if j != comm.rank:
+                #         comm.isend("found", dest=j)
+                #     break
+    else:
+        for line in data:
+            line = line.split("\t")
+            bdate = line[1].split("-")
+            today = date.today()
+            age = (
+                today.year
+                - int(bdate[0])
+                - ((today.month, today.day) < (int(bdate[1]), int(bdate[2])))
+            )
+            if zipCode == "":
+                if age >= minAge and age <= maxAge:
+                    if gender != 0:
 
+                        if gender == 1:
+                            cGender = "M"
+                        else:
+                            cGender = "F"
+
+                        if str(line[3]) == cGender:
+                            dict = {
+                                "Name": line[0],
+                                "Age": age,
+                                "Cnic": line[2],
+                                "Gender": line[3],
+                                "Street": line[5],
+                                "ZipCode": line[6],
+                                "Vaccinestatus": line[7],
+                                "CovidStatus": line[8],
+                            }
+                            userDetails.append(dict)
+                    else:
+                        dict = {
+                            "Name": line[0],
+                            "Age": age,
+                            "Cnic": line[2],
+                            "Gender": line[3],
+                            "Street": line[5],
+                            "ZipCode": line[6],
+                            "Vaccinestatus": line[7],
+                            "CovidStatus": line[8],
+                        }
+                        userDetails.append(dict)
+
+            if line[zipCodeIndex] == str(zipCode) and age >= minAge and age <= maxAge:
+                if gender != 0:
+
+                    if gender == 1:
+                        cGender = "M"
+                    else:
+                        cGender = "F"
+
+                    if str(line[3]) == cGender:
+                        dict = {
+                            "Name": line[0],
+                            "Age": age,
+                            "Cnic": line[2],
+                            "Gender": line[3],
+                            "Street": line[5],
+                            "ZipCode": line[6],
+                            "Vaccinestatus": line[7],
+                            "CovidStatus": line[8],
+                        }
+                        userDetails.append(dict)
+                else:
+                    dict = {
+                        "Name": line[0],
+                        "Age": age,
+                        "Cnic": line[2],
+                        "Gender": line[3],
+                        "Street": line[5],
+                        "ZipCode": line[6],
+                        "Vaccinestatus": line[7],
+                        "CovidStatus": line[8],
+                    }
+                    userDetails.append(dict)
 
     return userDetails
